f23e4b46a165f4363785c9d879a86a6a42874a75
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-02-11 02:06:19 -0300

    feat: limit formByUUID visibility for non-admins

--- src/schema/scholarship-form/query.ts
+++ src/schema/scholarship-form/query.ts
@@ -1,6 +1,7 @@
 import { schemaBuilder } from '@/schema/schema-builder';
 import { FormRef } from './ref';
 import { selectFormSchema } from '@/db/model/scholarship-form';
+import { ApiError } from '../api-error/ref';
 
 schemaBuilder.queryFields((t) => ({
     forms: t.field({
@@ -30,7 +31,7 @@ schemaBuilder.queryFields((t) => ({
         authz: {
             rules: ['IsAuthenticated'],
         },
-        resolve: async (parent, { uuid }, { DB }) => {
+        resolve: async (parent, { uuid }, { DB, STUDENT }) => {
             const result = await DB.query.formTable.findFirst({
                 where: (etc, { eq }) => {
                     return eq(etc.uuid, uuid);
@@ -41,20 +42,29 @@ schemaBuilder.queryFields((t) => ({
                 return null;
             }
 
-            // const now = new Date();
-            // if (STUDENT) {
-            //     if (!result.openDate || !result.closeDate) {
-            //         throw new Error('El formulario aún no está disponible');
-            //     }
+            const now = new Date();
+            if (STUDENT) {
+                if (!result.openDate || !result.closeDate) {
+                    throw new ApiError({
+                        code: 'FORM_NOT_AVAILABLE',
+                        message: 'El formulario no está disponible',
+                    });
+                }
 
-            //     if (result.openDate > now) {
-            //         throw new Error('El formulario aún no acepta respuestas');
-            //     }
+                if (result.openDate > now) {
+                    throw new ApiError({
+                        code: 'FORM_NOT_OPEN_YET',
+                        message: 'El formulario aún no está disponible',
+                    });
+                }
 
-            //     if (result.closeDate < now) {
-            //         throw new Error('El formulario ya no acepta respuestas');
-            //     }
-            // }
+                if (result.closeDate < now) {
+                    throw new ApiError({
+                        code: 'FORM_CLOSED',
+                        message: 'El formulario ya no está disponible',
+                    });
+                }
+            }
 
             return selectFormSchema.parse(result);
         },
